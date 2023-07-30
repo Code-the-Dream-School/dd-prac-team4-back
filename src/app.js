f3713f10a48996135640065a577bf446aacd4ea1
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-07-30 17:06:27 +0200

    added logging and security middleware to Express, winston framework installed,logs with logger.js created

--- src/app.js
+++ src/app.js
@@ -1,26 +1,27 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const express = require('express');
-const session = require('express-session');
-const app = express();
+const express = require('express')
+const session = require('express-session')
+const app = express()
 
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const favicon = require('express-favicon');
-const xss = require('xss-clean');
-const helmet = require('helmet');
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const favicon = require('express-favicon')
+const xss = require('xss-clean')
+const helmet = require('helmet')
 
-const passport = require('passport');
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', true);
-const asyncErrors = require('express-async-errors');
-const cookieParser = require('cookie-parser');
-const rateLimit = require('express-rate-limit');
+const passport = require('passport')
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', true)
+const asyncErrors = require('express-async-errors')
+const cookieParser = require('cookie-parser')
+const rateLimit = require('express-rate-limit')
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 60, // limit each IP to 100 requests per windowMs
-});
+  max: 60 // limit each IP to 100 requests per windowMs
+})
+const logger = require('./logger')
 
 // middleware setup
 // Configure express-session middleware
@@ -28,53 +29,58 @@ app.use(
   session({
     secret: 'your-secret-key', // Replace with a secret key for session data encryption
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: false
     // Other configuration options can be added as needed
   })
-);
+)
 
-app.use(cors());
 app.use(xss())
 
-app.use(express.json());
-app.use(helmet());
-app.use(express.urlencoded({ extended: false }));
-app.use(morgan('dev'));
-app.use(express.static('public'));
-app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(bodyParser.json());
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(cookieParser());
+app.use(express.json())
+//Security middleware
+app.use(helmet())
+app.use(cors())
+
+app.use(express.urlencoded({ extended: false }))
+
+//Logging middleware (using morgan)
+app.use(
+  morgan('dev', {
+    stream: { write: (message) => logger.info(message.trim()) }
+  })
+)
+app.use(express.static('public'))
+app.use(favicon(__dirname + '/public/favicon.ico'))
+app.use(bodyParser.json())
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(cookieParser())
 
 // Database setup (using Mongoose)
 
-
-
 const connectDB = (url) => {
-  return mongoose.connect(url);
-};
-
-
-
+  return mongoose.connect(url)
+}
 
 //routers
-const authRouter = require('./routes/authRoutes.js');
+const authRouter = require('./routes/authRoutes.js')
 
-app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/auth', authRouter)
 
 // Error handling middleware (must be defined after all other routes and middleware)
 //add later
 
 // Start the server
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URL);
-    app.listen(port, console.log(`Server is listening on port ${port}`));
+    await connectDB(process.env.MONGO_URL)
+    app.listen(port, () => {
+      logger.info(`Server running on http://localhost:${port}`)
+    })
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
 
-start();
+start()
