78f5c188050a31f6e0827daf03e12d01c88e4e74
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-08-01 19:52:44 +0200

    app.use(limiter) in app.js

--- src/app.js
+++ src/app.js
@@ -21,8 +21,10 @@ const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 60, // limit each IP to 100 requests per windowMs
 });
+
 const logger = require("./logger");
 
+app.use(limiter);
 // middleware setup
 
 app.use(xss());
