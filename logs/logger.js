40573fac1bcfb235cc1c67ebf364d431c3b8f41b
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-08-02 18:07:04 +0200

    edited logger.js, level.uppercase added

--- logs/logger.js
+++ logs/logger.js
@@ -1,21 +1,21 @@
 const winston = require('winston')
 
-// Define the log levels and colors (optional)
-const logLevels = {
-    error: 'red',
-    warn: 'yellow',
-    info: 'green',
-    debug: 'blue',
-}
+const capitalizeLevel = winston.format((info) => {
+    info.level = info.level.toUpperCase()
+    return info
+})
 
 winston.addColors(logLevels)
 
 const logger = winston.createLogger({
+    // level: "debug",
+    levels: logLevels,
     format: winston.format.combine(
+        capitalizeLevel(),
         winston.format.colorize(),
         winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
         winston.format.printf(({ timestamp, level, message }) => {
-            return `${timestamp} [${level.toUpperCase()}]: ${message}`
+            return `${timestamp} [${level}]: ${message}`
         })
     ),
     transports: [
