a92975c588405f8b17d7e798cbe191bc37e49588
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-08-02 17:59:28 +0200

    updated logger.js code, removd the level option from the createLogger function

--- logs/logger.js
+++ logs/logger.js
@@ -1,30 +1,27 @@
-// logger.js
-const winston = require("winston");
+const winston = require('winston')
 
 // Define the log levels and colors (optional)
 const logLevels = {
-  error: "red",
-  warn: "yellow",
-  info: "green",
-  debug: "blue",
-};
+    error: 'red',
+    warn: 'yellow',
+    info: 'green',
+    debug: 'blue',
+}
 
-winston.addColors(logLevels);
+winston.addColors(logLevels)
 
 const logger = winston.createLogger({
-  level: "debug",
-  levels: logLevels,
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-    winston.format.printf(({ timestamp, level, message }) => {
-      return `${timestamp} [${level.toUpperCase()}]: ${message}`;
-    }),
-  ),
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: "logs/app.log" }),
-  ],
-});
+    format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        winston.format.printf(({ timestamp, level, message }) => {
+            return `${timestamp} [${level.toUpperCase()}]: ${message}`
+        })
+    ),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'logs/app.log' }),
+    ],
+})
 
-module.exports = logger;
+module.exports = logger
