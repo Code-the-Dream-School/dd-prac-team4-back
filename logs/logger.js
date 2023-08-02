cdbceb8eb06c70fb5ecd5f5e18cc2d25cf496bf8
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-08-02 15:32:37 +0200

    new file:   logs/logger.js

---
+++ logs/logger.js
@@ -0,0 +1,30 @@
+// logger.js
+const winston = require("winston");
+
+// Define the log levels and colors (optional)
+const logLevels = {
+  error: "red",
+  warn: "yellow",
+  info: "green",
+  debug: "blue",
+};
+
+winston.addColors(logLevels);
+
+const logger = winston.createLogger({
+  level: "debug",
+  levels: logLevels,
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    winston.format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+    }),
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "logs/app.log" }),
+  ],
+});
+
+module.exports = logger;
