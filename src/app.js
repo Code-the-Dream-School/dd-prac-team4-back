29537938d7eeb13ee49abc5790edae8da5475fdb
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-08-16 16:11:28 +0200

    removed extra route for album endpoint

--- src/app.js
+++ src/app.js
@@ -75,9 +75,6 @@ app.get('/health', (req, res) => {
   res.status(200).send('OK');
 });
 
-// Use the albumRouter for /albums endpoint
-app.use('/albums', albumRouter);
-
 // Error handling middleware (must be defined after all other routes and middleware)
 app.use(notFoundMiddleware); // Not found middleware to handle invalid routes
 app.use(errorHandlerMiddleware); // Error handler middleware
