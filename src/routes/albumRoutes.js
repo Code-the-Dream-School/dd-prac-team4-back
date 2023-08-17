20fd3309e6c786d10d6e0abd149893e9bd1d9293
Natacha Key <nataliaklueva@hotmail.com>
2023-08-17 12:26:05 +0200

    updated albumRoutes.js

--- src/routes/albumRoutes.js
+++ src/routes/albumRoutes.js
@@ -26,11 +26,15 @@ router.get('/filter', getFilteredAlbums);
 router
   .route('/:id')
   .get(getSingleAlbum) //everyone can access all products- no middleware
+  .patch(authenticateUser, authorizePermissions('admin'), updateAlbum); //only admin can update album
+
+
+  router
+  .route('/:id/listOfUsersWhoPurchasedThisAlbum')
   .get(
     authenticateUser,
     authorizePermissions('admin'),
     getAlbumWithAllUsersWhoPurchasedIt
-  ) //only admin can see who purchased albm
-  .patch(authenticateUser, authorizePermissions('admin'), updateAlbum); //only admin can update album
+  ) //only admin can see who purchased album
 
 module.exports = router;
