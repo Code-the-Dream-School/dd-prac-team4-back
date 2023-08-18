060fd37f44231c1b3b5fe8a60d038d1a3d7a86e6
Marina Tilton <m.v.stoyan@gmail.com>
2023-08-18 12:28:19 -0700

    orders schema fixed

--- src/models/Orders.js
+++ src/models/Orders.js
@@ -16,23 +16,19 @@ const orderItemSchema = new mongoose.Schema({
   },
 });
 
-// Create the OrderItem model using the defined schema
-const OrderItem = mongoose.model('OrderItem', orderItemSchema);
-
 // Schema for the main Order model
 const orderSchema = new mongoose.Schema({
     // The user field references the User model and is required
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User', // Referencing the User model
-    required: true,
+    required: [true, 'Please provide user'],
   },
   // The orderStatus field indicates the current status of the order
   orderStatus: {
     type: String,
     enum: ['pending', 'payment_successful', 'payment_failed', 'cancelled', 'complete'],
     required: true, // Order status is required
-    default: 'pending' // Set initial order status as pending
   },
   // The tax field represents the tax percentage applied to the order
   tax: {
@@ -45,60 +41,46 @@ const orderSchema = new mongoose.Schema({
   subtotal: {
     type: Number,
     required: true,
+    min: 0,
   },
   // The total field represents the total price after taxes
   total: {
     type: Number,
     required: true,
+    min: 0,
   },
   // The paymentIntentId field stores the Stripe state of the purchase
   paymentIntentId: String, // Payment intent ID is a string
+  orderItems: [orderItemSchema],
 }, {
   timestamps: true, // Automatically add createdAt and updatedAt timestamps
 });
 
-// Create a function to update order statuses
-const updateOrderStatus = async () => {
-  try {
-    // Get the current date and subtract 1 minute
-    const oneMinutesAgo = new Date(Date.now() - 1 * 60 * 1000);
-    
-    // Update orders with a status of "pending" that were created 1 minute ago or earlier,
-    // and set the order status to "cancelled"
-    const result = await Order.updateMany(
-      { orderStatus: 'pending', createdAt: { $lte: oneMinutesAgo } },
+// Middleware: Update order status for pending orders older than 10 minutes
+orderSchema.pre('find', async function (next) {
+    // Create a date object representing the time 10 minutes ago
+    const tenMinutesAgo = new Date(Date.now() - 10 * 60 * 1000);
+    // Update all orders with a "pending" status and that were created before or equal to 10 minutes ago
+    await this.updateMany(
+      { orderStatus: 'pending', createdAt: { $lte: tenMinutesAgo } },
+      { $set: { orderStatus: 'cancelled' } } // Set the order status to "cancelled"
+    );
+    next();
+  });
+  
+  // updates the status of orders with a "pending" status that were created more 
+  // than 10 minutes ago to "cancelled". This happens before executing a findOne operation.
+  orderSchema.pre('findOne', async function (next) {
+    // Get the current date and subtract 10 minutes
+    const tenMinutesAgo = new Date(Date.now() - 10 * 60 * 1000);
+    // Find orders with "pending" status created more than 10 minutes ago
+  // and change their status to "cancelled"
+    await this.updateMany(
+      { orderStatus: 'pending', createdAt: { $lte: tenMinutesAgo } },
       { $set: { orderStatus: 'cancelled' } }
     );
-    console.log('Update result:', result);
-  } catch (error) {
-    console.error('Error updating orders:', error);
-  }
-};
-
-// Call the updateOrderStatus function every 1 minute (60 * 1000 milliseconds)
-setInterval(updateOrderStatus, 1 * 60 * 1000);
-
-// Middleware: Update order statuses before executing a find operation
-orderSchema.pre('find', async function (next) {
-  console.log('Pre-find started');
-  
-  // Call the updateOrderStatus function to update order statuses
-  await updateOrderStatus();
-  
-  console.log('Pre-find finished');
-  next();
-});
-
-// Middleware: Update order statuses before executing a findOne operation
-orderSchema.pre('findOne', async function (next) {
-  console.log('Pre-findOne started');
-  
-  // Call the updateOrderStatus function to update order statuses
-  await updateOrderStatus();
-  
-  console.log('Pre-findOne finished');
-  next();
-});
+    next();
+  });
 
 const Order = mongoose.model('Order', orderSchema);
 
