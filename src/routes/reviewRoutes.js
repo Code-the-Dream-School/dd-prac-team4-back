81c4eea59c1cf75d2410b66eb89f66dc64a96c96
Natacha Key <nataliaklueva@hotmail.com>
2023-08-20 17:55:48 +0200

    deleted some comments

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -11,6 +11,5 @@ const {
 
 router.route('/').post(authenticateUser, createReview).get(getAllReviews);
 
-//? for Akos- does it matter which type of request we add here- put or patch? because I used put but it didn't overwright the id of the review
 router.route('/:id').patch(authenticateUser, updateReview).get(getSingleReview)
 module.exports = router;
