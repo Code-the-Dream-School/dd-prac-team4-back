71a4cc9dd743548f43c63e3b42c4ca1a4dd7a58c
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-08-22 19:20:33 +0200

    wishlistController.js added

---
+++ src/controllers/wishlistController.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const Wishlist = require('./wishlist');
+
+// Add an album to a wishlist
+async function addAlbumToWishlist(req, res) {
+  try {
+    const { wishlist_id, album_id } = req.params;
+
+    if (
+      !mongoose.Types.ObjectId.isValid(wishlist_id) ||
+      !mongoose.Types.ObjectId.isValid(album_id)
+    ) {
+      return res.status(400).json({ error: 'Invalid ID format' });
+    }
+
+    const wishlist = await Wishlist.findByIdAndUpdate(
+      wishlist_id,
+      { $addToSet: { albums: album_id } },
+      { new: true }
+    ).populate('albums');
+
+    if (!wishlist) {
+      return res.status(404).json({ error: 'Wishlist not found' });
+    }
+
+    res.json(wishlist);
+  } catch (error) {
+    res.status(500).json({ error: 'Could not update wishlist' });
+  }
+}
+
+// Remove an album from a wishlist
+async function removeAlbumFromWishlist(req, res) {
+  try {
+    const { wishlist_id, album_id } = req.params;
+
+    if (
+      !mongoose.Types.ObjectId.isValid(wishlist_id) ||
+      !mongoose.Types.ObjectId.isValid(album_id)
+    ) {
+      return res.status(400).json({ error: 'Invalid ID format' });
+    }
+
+    const wishlist = await Wishlist.findByIdAndUpdate(
+      wishlist_id,
+      { $pull: { albums: album_id } },
+      { new: true }
+    ).populate('albums');
+
+    if (!wishlist) {
+      return res.status(404).json({ error: 'Wishlist not found' });
+    }
+
+    res.json(wishlist);
+  } catch (error) {
+    res.status(500).json({ error: 'Could not update wishlist' });
+  }
+}
+
+module.exports = {
+  addAlbumToWishlist,
+  removeAlbumFromWishlist,
+};
