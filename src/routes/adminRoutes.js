2ba267a2a2a2dfede382ef918c3189bf5abb50c1
Marina Tilton <m.v.stoyan@gmail.com>
2023-08-24 12:08:43 -0700

    admin page style

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -5,10 +5,12 @@ const adminController = require('../controllers/adminController');
 
 router.get('/', adminController.adminHomePage);
 
-router.use(expressBasicAuth({
-  users: { [process.env.ADMIN_USERNAME]: process.env.ADMIN_PASSWORD },
-  challenge: true,
-  realm: 'admin site'
-}));
+router.use(
+  expressBasicAuth({
+    users: { [process.env.ADMIN_USERNAME]: process.env.ADMIN_PASSWORD },
+    challenge: true,
+    realm: 'admin site',
+  })
+);
 
 module.exports = router;
