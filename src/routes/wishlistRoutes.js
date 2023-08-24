ce1cb120f0abd9d316cb3788520c6708e76ce415
Yelena Japarova <88336329+DYA13@users.noreply.github.com>
2023-08-24 16:20:45 +0200

    Update src/routes/wishlistRoutes.js
    Co-authored-by: Akosua <9094098+akosasante@users.noreply.github.com>

--- src/routes/wishlistRoutes.js
+++ src/routes/wishlistRoutes.js
@@ -9,41 +9,6 @@ const mongoose = require('mongoose');
 
 router.patch('/wishlist/:wishlist_id/add_album/:album_id', addAlbumToWishlist);
 
-router.patch(
-  '/wishlist/:wishlist_id/remove_album/:album_id',
-  async (req, res) => {
-    const { wishlist_id, album_id } = req.params;
-
-    try {
-      if (
-        !mongoose.Types.ObjectId.isValid(wishlist_id) ||
-        !mongoose.Types.ObjectId.isValid(album_id)
-      ) {
-        return res.status(400).send('Invalid wishlist_id or album_id');
-      }
-
-      // Find the Wishlist document
-      const wishlist = await Wishlist.findById(wishlist_id);
-
-      // Check if wishlist exists
-      if (!wishlist) {
-        return res.status(404).send('Wishlist not found');
-      }
-
-      // Check if album is in wishlist
-      if (!wishlist.albums.includes(album_id)) {
-        return res.status(200).json(wishlist);
-      }
-
-      // Remove album from wishlist and save
-      wishlist.albums.pull(album_id);
-      await wishlist.save();
-
-      return res.status(200).json(wishlist);
-    } catch (error) {
-      return res.status(500).send('Server error');
-    }
-  }
-);
+router.patch('/wishlist/:wishlist_id/remove_album/:album_id', removeAlbumFromWishlist);
 
 module.exports = router;
