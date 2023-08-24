81a04e4c777445f0c423492654bf1c466db5fcf3
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-08-24 18:50:07 +0200

    const mongoose and wishlist removed from wishlistRoutes.js

--- src/routes/wishlistRoutes.js
+++ src/routes/wishlistRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const Wishlist = require('../models/Wishlist');
 const {
   addAlbumToWishlist,
   removeAlbumFromWishlist,
 } = require('../controllers/wishlistController');
+
 const router = express.Router();
-const mongoose = require('mongoose');
 
 router.patch('/wishlist/:wishlist_id/add_album/:album_id', addAlbumToWishlist);
 
