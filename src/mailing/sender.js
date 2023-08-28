9a9c6e4ab20943a856ddb103b616c9ee470f7370
Marina Tilton <m.v.stoyan@gmail.com>
2023-08-28 13:52:19 -0700

    test endpoint that sends a basic email

--- src/mailing/sender.js
+++ src/mailing/sender.js
@@ -6,26 +6,29 @@ const transport = nodemailer.createTransport({
   service: process.env.EMAIL_SERVICE,
   auth: {
     user: process.env.EMAIL_USERNAME, // Your Outlook email address
-    pass: process.env.EMAIL_PASSWORD // Your Outlook password
-  }
+    pass: process.env.EMAIL_PASSWORD, // Your Outlook password
+  },
 });
 
 // Function to send a test email (promisified)
 async function sendTestEmail(recipient) {
   return new Promise((resolve, reject) => {
-    transport.sendMail({
-      from: process.env.EMAIL_SENDER,
-      to: recipient, // Вместо process.env.RECIPIENT_EMAIL используйте аргумент recipient
-      subject: 'Test Email',
-      text: 'Hello, this is a test email!',
-      html: '<p>Hello, this is a test email!</p>'
-    }, (err, res) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(res);
+    transport.sendMail(
+      {
+        from: process.env.EMAIL_USERNAME,
+        to: recipient, // Вместо process.env.RECIPIENT_EMAIL используйте аргумент recipient
+        subject: 'Test Email',
+        text: 'Hello, this is a test email!',
+        html: '<p>Hello, this is a test email!</p>',
+      },
+      (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res);
+        }
       }
-    });
+    );
   });
 }
 
