8d6a79d3c57fbf71ce2dff6cfe653f6613b5d8b6
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-08-29 19:45:23 +0200

    edited sender.js

--- src/email/sender.js
+++ src/email/sender.js
@@ -1,16 +1,28 @@
 const nodemailer = require('nodemailer');
+const path = require('path');
+const ejs = require('ejs');
 
 async function sendForgotPasswordEmail(toEmail, resetToken) {
   const transporter = nodemailer.createTransport({
-    // Configure your email service here
+    service: 'gmail',
+    auth: {
+      user: 'your.email@gmail.com',
+      pass: 'your-email-password',
+    },
   });
+
+  // Load and render the HTML template
+  const htmlTemplate = await ejs.renderFile(
+    path.join(__dirname, '../email/templates/forgotPassword.html.ejs'), // Adjust the path
+    { resetLink: `http://yourapp.com/reset-password/${resetToken}` } // Adjust the URL structure
+  );
 
   const mailOptions = {
     from: 'your@example.com',
     to: toEmail,
     subject: 'Password Reset Request',
-    html: // Load and render the forgotPassword.html.ejs template here,
-   };
+    html: htmlTemplate,
+  };
 
   try {
     await transporter.sendMail(mailOptions);
