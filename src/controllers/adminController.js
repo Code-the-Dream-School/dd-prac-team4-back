5cddedcc5b186561d6cf531dea57613b188d8e59
Marina Tilton <m.v.stoyan@gmail.com>
2023-09-01 00:07:02 -0700

    swagger.tags Admin

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,6 +1,13 @@
 const Album = require('../models/Album');
 
 const adminHomePage = async (req, res) => {
+  /*
+    #swagger.summary: Update prices of albums
+    #swagger.description: Update the prices of selected albums.
+    #swagger.responses[200]: { description: 'Successful response with updated albums.' }
+    #swagger.responses[400]: { description: 'Bad request, invalid input.' }
+    #swagger.responses[500]: { description: 'Internal server error' }
+  */
   const allAlbums = await Album.find({}).sort({ price: 1 }); // Fetch all albums from the database and sort them by price in ascending order
   res.render('adminPage', { albums: allAlbums });
 };
