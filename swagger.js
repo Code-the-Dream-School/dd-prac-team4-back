3a88a8e807383e740913319bc5e3acd31326dfa7
Natalia <nataliaklueva@hotmail.com>
2023-09-01 16:09:28 +0200

    new swagger.js

--- swagger.js
+++ swagger.js
@@ -126,5 +126,4 @@ const routes = ['./src/expressServer.js'];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
-
 swaggerAutogen({ openapi: '3.1.0' })(outputFile, routes, doc);
