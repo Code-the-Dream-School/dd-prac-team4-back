887a9011f9ee442dbe445df9f9ae823382d747ee
Marina Tilton <m.v.stoyan@gmail.com>
2023-09-03 15:57:08 -0700

    bug fixed

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,30 +5,6 @@ const CustomError = require('../errors');
 const { attachCookiesToResponse, createTokenUser } = require('../utils');
 
 const register = async (req, res) => {
-  /* 
-    #swagger.summary = 'Register a new user'
-    #swagger.parameters['user'] = {
-      in: 'body',
-      description: 'User registration information',
-      required: true,
-      schema: {
-        $name: "John Doe",
-        $email: "jdoe@example.com",
-        $password: "supersecretpassword",
-        $username: "jdoe99"
-      }
-    }
-    #swagger.responses[201] = {
-      description: 'User registered successfully',
-      schema: {
-        $ref: '#/definitions/TokenizedUser'
-      }
-    }
-    #swagger.responses[400] = {
-      description: 'Bad request, missing email, name, password, or username'
-    }
-  */
-
   const { name, email, password, username } = req.body;
 
   if (!email || !name || !password || !username) {
@@ -54,9 +30,51 @@ const register = async (req, res) => {
   attachCookiesToResponse({ res, user: tokenUser }); // Attach the token to cookies and send in the response
 
   res.status(StatusCodes.CREATED).json({ user: tokenUser }); // Send a successful response with user data
+  /* 
+    #swagger.summary = 'Register a new user'
+    #swagger.parameters['user'] = {
+      in: 'body',
+      description: 'User registration information',
+      required: true,
+      schema: {
+        $name: "John Doe",
+        $email: "jdoe@example.com",
+        $password: "supersecretpassword",
+        $username: "jdoe99"
+      }
+    }
+    #swagger.responses[201] = {
+      description: 'User registered successfully',
+      schema: {
+        $ref: '#/definitions/TokenizedUser'
+      }
+    }
+    #swagger.responses[400] = {
+      description: 'Bad request, missing email, name, password, or username'
+    }
+  */
 };
 
 const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new CustomError.BadRequestError('Please provide email and password'); // If either field is not provided, throw an error
+  }
+  const user = await User.findOne({ email }); // Find a user by email
+
+  if (!user) {
+    throw new CustomError.UnauthenticatedError('Invalid Credentials');
+  }
+
+  const isPasswordValid = await argon2.verify(user.password, password); // Check password validity
+  if (!isPasswordValid) {
+    throw new CustomError.UnauthenticatedError('Invalid Credentials'); // If password is incorrect, throw an error
+  }
+  const tokenUser = createTokenUser(user);
+  attachCookiesToResponse({ res, user: tokenUser });
+
+  res.status(StatusCodes.CREATED).json({ user: tokenUser });
   /* 
     #swagger.summary = 'Login as a user'
     #swagger.parameters['user'] = {
@@ -81,29 +99,16 @@ const login = async (req, res) => {
       description: 'Unauthorized, invalid credentials'
     }
   */
-  const { email, password } = req.body;
-
-  if (!email || !password) {
-    throw new CustomError.BadRequestError('Please provide email and password'); // If either field is not provided, throw an error
-  }
-  const user = await User.findOne({ email }); // Find a user by email
-
-  if (!user) {
-    throw new CustomError.UnauthenticatedError('Invalid Credentials');
-  }
-
-  const isPasswordValid = await argon2.verify(user.password, password); // Check password validity
-  if (!isPasswordValid) {
-    throw new CustomError.UnauthenticatedError('Invalid Credentials'); // If password is incorrect, throw an error
-  }
-  const tokenUser = createTokenUser(user);
-  attachCookiesToResponse({ res, user: tokenUser });
-
-  res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
 
 //logout endpoint
 const logout = async (req, res) => {
+  res.clearCookie('token', {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+
+  res.status(StatusCodes.OK).json({ msg: 'user logged out!' });
   /*
     #swagger.summary = 'Logout a user'
     #swagger.description = 'This endpoint checks for a signed JWT token in an HTTP-only cookie and clears it. It checks the `token` field in the cookie.'
@@ -113,12 +118,6 @@ const logout = async (req, res) => {
     }
     #swagger.security = [{ "JWT": [] }]
   */
-  res.clearCookie('token', {
-    httpOnly: true,
-    expires: new Date(Date.now()),
-  });
-
-  res.status(StatusCodes.OK).json({ msg: 'user logged out!' });
 };
 
 module.exports = {
