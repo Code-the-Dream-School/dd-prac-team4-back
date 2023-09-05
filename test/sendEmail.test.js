5b3d13dc715ad88083801bd0653ce24a8b9ee218
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-09-05 23:39:14 +0200

    sendEmail.test.js edited

--- test/sendEmail.test.js
+++ test/sendEmail.test.js
@@ -1,7 +1,7 @@
-const sendTestEmail = require('../path/to/sendTestEmail'); // Replace with the actual path
+const sendTestEmail = require('/mailing/sender.js');
 describe('sendTestEmail function', () => {
   it('sends an email with expected content', async () => {
-    const result = await sendTestEmail(); // Replace with your actual inputs
+    const result = await sendTestEmail();
     expect(result.originalMessage.from).toEqual('expectedFromValue');
     expect(result.originalMessage.to).toEqual('expectedToValue');
   });
