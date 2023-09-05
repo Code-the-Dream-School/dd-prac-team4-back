ef22adbf4473cd09a28dba0a7a1ae5139cbdae02
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-09-05 15:54:41 +0200

    the function sendForgotPasswordEmail edited in mailing/sender.js

--- src/mailing/sender.js
+++ src/mailing/sender.js
@@ -1,4 +1,7 @@
 const nodemailer = require('nodemailer');
+const path = require('path');
+const ejs = require('ejs');
+require('dotenv').config();
 
 // transport using Nodemailer
 const transport = nodemailer.createTransport({
@@ -31,4 +34,34 @@ async function sendTestEmail(recipient) {
   });
 }
 
-module.exports = sendTestEmail;
+async function sendForgotPasswordEmail(toEmail, resetToken) {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  // Load and render the HTML template
+  const htmlTemplate = await ejs.renderFile(
+    path.join(__dirname, '../email/templates/html.ejs'),
+    { resetLink: `http://localhost:8000/api/v1/reset-password/${resetToken}` }
+  );
+
+  const mailOptions = {
+    from: 'your@example.com',
+    to: toEmail,
+    subject: 'Password Reset Request',
+    html: htmlTemplate,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log('Password reset email sent successfully');
+  } catch (error) {
+    console.error('Error sending password reset email:', error);
+  }
+}
+
+module.exports = { sendTestEmail, sendForgotPasswordEmail };
