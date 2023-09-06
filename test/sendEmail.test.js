ae0d8e079665e62071dbe8bdca4afee28e13e348
Yelena Japarova <88336329+DYA13@users.noreply.github.com>
2023-09-06 23:07:08 +0200

    Update test/sendEmail.test.js
    Co-authored-by: Akosua <9094098+akosasante@users.noreply.github.com>

--- test/sendEmail.test.js
+++ test/sendEmail.test.js
@@ -10,10 +10,7 @@ describe('sendTestEmail function', () => {
     const result = await sendTestEmail(recipientEmail);
 
     // Verify the content of the sent email
-    expect(result.from).toEqual(process.env.EMAIL_USERNAME); // Verify the 'from' address
-    expect(result.to).toEqual(recipientEmail); // Verify the 'to' address
-    expect(result.subject).toEqual('Test Email'); // Verify the subject
-    expect(result.text).toEqual('Hello, this is a test email!'); // Verify the plain text content
-    expect(result.html).toEqual('<p>Hello, this is a test email!</p>'); // Verify the HTML content
+    expect(result.envelope.from).toEqual(process.env.EMAIL_USERNAME); // Verify the 'from' address
+    expect(result.envelope.to).toEqual(recipientEmail); // Verify the 'to' address
   });
 });
