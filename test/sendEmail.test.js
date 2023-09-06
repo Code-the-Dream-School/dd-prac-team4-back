fcc322e040a12a3b6c936731105686ec4ceec93c
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-09-06 11:11:37 +0200

    sendTestEmail function edited in test/sendEmail.test.js

--- test/sendEmail.test.js
+++ test/sendEmail.test.js
@@ -1,8 +1,18 @@
-const sendTestEmail = require('/mailing/sender.js');
+const sendTestEmail = require('../mailing/sender.js');
+
 describe('sendTestEmail function', () => {
-  it('sends an email with expected content', async () => {
-    const result = await sendTestEmail();
-    expect(result.originalMessage.from).toEqual('expectedFromValue');
-    expect(result.originalMessage.to).toEqual('expectedToValue');
+  it('sends a test email with expected content', async () => {
+    // Define the recipient's email address
+    const recipientEmail = 'recipient@example.com';
+
+    // Send the test email
+    const result = await sendTestEmail(recipientEmail);
+
+    // Verify the content of the sent email
+    expect(result.from).toEqual(process.env.EMAIL_USERNAME); // Verify the 'from' address
+    expect(result.to).toEqual(recipientEmail); // Verify the 'to' address
+    expect(result.subject).toEqual('Test Email'); // Verify the subject
+    expect(result.text).toEqual('Hello, this is a test email!'); // Verify the plain text content
+    expect(result.html).toEqual('<p>Hello, this is a test email!</p>'); // Verify the HTML content
   });
 });
