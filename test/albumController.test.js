2e0720dc421666005d98c2bd24d631e66646ade7
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-09-18 22:07:49 +0200

    added code to albumController.test.js

--- test/albumController.test.js
+++ test/albumController.test.js
@@ -4,13 +4,12 @@ const request = require('supertest');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 const Album = require('../src/models/Album');
 
-// Import the AlbumController methods you want to test
 const {
   getAllAlbums,
   getSingleAlbum,
   getAlbumWithAllUsersWhoPurchasedIt,
   getFilteredAlbums,
-} = require('../src/albumControllercontrollers/albumController');
+} = require('../src/controllers/albumController');
 
 let server;
 let mongooseConnection;
@@ -29,11 +28,13 @@ afterAll(async () => {
   await mongooseConnection.disconnect();
   await mongodb.stop();
 });
-describe('GET /api/v1/albums endpoint', () => {
+
+describe('AlbumController API Tests', () => {
   const mockAlbumData = {
     albumName: 'Test Album',
     artistName: 'Test Artist',
     price: 9.99,
+    spotifyUrl: 'https://api.spotify.com/v1/albums/',
   };
 
   beforeEach(async () => {
@@ -41,23 +42,33 @@ describe('GET /api/v1/albums endpoint', () => {
     await Album.create(mockAlbumData);
   });
 
-  it('should test the getAllAlbums endpoint', async () => {
-    const response = await request(app).get('/api/albums');
+  it('should test the getAllAlbums endpoint - Success Case', async () => {
+    const response = await request(app).get('/api/v1/albums');
     expect(response.status).toBe(StatusCodes.OK);
     expect(response.body).toHaveProperty('albums');
     expect(response.body.albums).toHaveLength(1);
   });
 
-  it('should test the getSingleAlbum endpoint', async () => {
+  it('should test the getAllAlbums endpoint - Error Case (Unauthorized)', async () => {
+    const response = await request(app).get('/api/v1/albums');
+    expect(response.status).toBe(StatusCodes.UNAUTHORIZED);
+  });
+
+  it('should test the getSingleAlbum endpoint - Success Case', async () => {
     const albums = await Album.find({});
     const albumId = albums[0]._id;
 
-    const response = await request(app).get(`/api/albums/${albumId}`);
+    const response = await request(app).get(`/api/albums/v1/${albumId}`);
     expect(response.status).toBe(StatusCodes.OK);
     expect(response.body).toHaveProperty('album');
   });
 
-  it('should test the getAlbumWithAllUsersWhoPurchasedIt endpoint', async () => {
+  it('should test the getSingleAlbum endpoint - Error Case (Not Found)', async () => {
+    const response = await request(app).get('/api/albums/v1/invalidAlbumId');
+    expect(response.status).toBe(StatusCodes.NOT_FOUND);
+  });
+
+  it('should test the getAlbumWithAllUsersWhoPurchasedIt endpoint - Success Case', async () => {
     const albums = await Album.find({});
     const albumId = albums[0]._id;
 
@@ -69,10 +80,26 @@ describe('GET /api/v1/albums endpoint', () => {
     expect(response.body).toHaveProperty('purchasingUsersCount');
   });
 
-  it('should test the getFilteredAlbums endpoint', async () => {
-    const response = await request(app).get('/api/albums?limit=10&order=asc');
+  it('should test the getAlbumWithAllUsersWhoPurchasedIt endpoint - Error Case (Not Found)', async () => {
+    const response = await request(app).get(
+      '/api/albums/invalidAlbumId/purchased-users'
+    );
+    expect(response.status).toBe(StatusCodes.NOT_FOUND); // Expect not found status code
+  });
+
+  it('should test the getFilteredAlbums endpoint - Success Case', async () => {
+    const response = await request(app).get(
+      '/api/v1/albums?limit=10&order=asc'
+    );
     expect(response.status).toBe(StatusCodes.OK);
     expect(response.body).toHaveProperty('albums');
     expect(response.body).toHaveProperty('count');
+  });
+
+  it('should test the getFilteredAlbums endpoint - Error Case (Bad Request)', async () => {
+    const response = await request(app).get(
+      '/api/v1/albums?invalidParam=invalidValue'
+    );
+    expect(response.status).toBe(StatusCodes.BAD_REQUEST); // Expect bad request status code
   });
 });
