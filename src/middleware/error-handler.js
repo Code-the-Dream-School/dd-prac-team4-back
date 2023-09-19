842ff44a52cb9ab34c254db5d22dc755cacd014f
Akosua Asante <9094098+akosasante@users.noreply.github.com>
2023-09-19 13:20:39 -0400

    error handler tweak

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -2,11 +2,20 @@ const { StatusCodes } = require('http-status-codes');
 const logger = require('../../logs/logger');
 
 const errorHandlerMiddleware = (err, req, res, _next) => {
-  logger.error(err);
+  if (process.env.NODE_ENV === 'test') {
+    // in test mode only log the message to make the test output less verbose
+    logger.error(err.message);
+  } else {
+    // log the full error stack only outside of test mode
+    logger.error(err);
+  }
+
   let customError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || 'Something went wrong try again later',
   };
+
+  // handle different mongoose error codes
   if (err.name === 'ValidationError') {
     customError.msg = Object.values(err.errors)
       .map((item) => item.message)
@@ -24,6 +33,7 @@ const errorHandlerMiddleware = (err, req, res, _next) => {
     customError.statusCode = 404;
   }
 
+  // return an error response
   return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
