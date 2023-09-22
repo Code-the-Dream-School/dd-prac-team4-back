8bf38475e4e72c1cd846926a9b90cc2b1f007888
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-09-22 23:49:28 +0200

    loginAndReturnCookie imported from test_helper in authController.test.js, userController.test.js

--- test/authController.test.js
+++ test/authController.test.js
@@ -3,6 +3,7 @@ const { MongoMemoryServer } = require('mongodb-memory-server');
 const request = require('supertest');
 const { intervalId: orderUpdateInterval } = require('../src/models/Order');
 const User = require('../src/models/User');
+const { loginAndReturnCookie } = require('./test_helper');
 
 // Declare variables for the server, database connection, and in-memory MongoDB instance
 let server;
@@ -22,21 +23,6 @@ const testUser = {
   email: 'ava@ava.com',
   password: 'secret',
   role: 'user',
-};
-
-// log in and return a cookie
-const loginAndReturnCookie = async (credentials) => {
-  // Send a login request and get the signed cookie from the response
-  const resp = await request(app).post('/api/v1/auth/login').send(credentials);
-
-  // Get the 'set-cookie' header from the response
-  const cookieHeader = resp.headers['set-cookie'];
-  // Find the cookie that starts with 'token' in the 'set-cookie' header
-  // Split the cookie string by ';' and take the first part (before the first ';')
-  const signedCookie = cookieHeader
-    .find((cookie) => cookie.startsWith('token'))
-    .split(';')[0];
-  return signedCookie; // Return the signed cookie, which typically contains authentication token
 };
 
 // set up the mongodb and the express server before starting the tests
