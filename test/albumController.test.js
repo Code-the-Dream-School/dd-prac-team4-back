4633d56e3cb7cb7fbf6e74e80f787c8c7bdaa14f
Yelena Japarova <88336329+DYA13@users.noreply.github.com>
2023-09-22 23:08:24 +0200

    Update test/albumController.test.js
    Co-authored-by: Akosua <9094098+akosasante@users.noreply.github.com>

--- test/albumController.test.js
+++ test/albumController.test.js
@@ -172,7 +172,7 @@ describe('AlbumController API Tests', () => {
 
     await Album.insertMany(albumsToCreate);
 
-    // Make a GET request to the /api/v1/albums endpoint with a limit parameter set to 2
+    // Make a GET request to the /api/v1/albums/filter endpoint with a limit parameter set to 2
     const limit = 2;
     const response = await request(app).get(`/api/v1/albums/filter?limit=${limit}`);
 
