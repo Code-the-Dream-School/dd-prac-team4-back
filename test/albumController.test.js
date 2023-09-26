2cb2ecd237dc13b8d6c8b3c04eaddcac5b6345a6
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-09-26 23:11:22 +0200

    removed bad request status

--- test/albumController.test.js
+++ test/albumController.test.js
@@ -163,9 +163,7 @@ describe('AlbumController API Tests', () => {
     const response = await request(app).get(
       `/api/v1/albums/filter?limit=${limit}`
     );
-    expect(response.status).toBe(StatusCodes.BAD_REQUEST);
-
-
+  
     expect(response.status).toBe(StatusCodes.OK);
     expect(response.body).toHaveProperty('albums');
     expect(response.body).toHaveProperty('count');
