1b4a2c1f6aa7c7954d2a39203862bed3d558f155
Marina Tilton <m.v.stoyan@gmail.com>
2023-10-08 18:05:00 -0700

    display username

--- src/live/handleAlbumChat.js
+++ src/live/handleAlbumChat.js
@@ -7,9 +7,16 @@ async function handleAlbumChat(io, socket, data) {
     // Create a chat room name for the album chat
     const chatRoomName = `chat:album:${albumId}`;
 
-    const msg = await ChatMessage.create({ ...data, user: data.user });
-    await msg.populate('user', 'name');
-    console.log('Chat message saved:', data);
+    const msg = await ChatMessage.create({ ...data, username: data.user });
+    await msg.populate('user', 'username');
+    console.log('Chat message saved:', msg);
+
+    // Return the _id of the saved message
+    const savedMessage = {
+      ...data,
+      _id: msg._id, // Include _id in the message
+      username: data.username,
+    };
 
     // Check and delete old messages if needed
     const messagesCount = await ChatMessage.countDocuments({ albumId });
@@ -21,7 +28,7 @@ async function handleAlbumChat(io, socket, data) {
     }
 
     // Broadcast the message to all connected users in the album chat room
-    io.to(chatRoomName).emit('chat:album', data);
+    io.to(chatRoomName).emit('chat:album', savedMessage); 
   } catch (error) {
     console.error('Error handling chat message:', error);
   }
