53da59258d93532b895fd32c24dc827c0093082b
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-10-11 10:48:39 +0200

    deleted line 29-34 for test 'sends a test email with expected content' in sendEmail.test.js

--- test/sendEmail.test.js
+++ test/sendEmail.test.js
@@ -26,11 +26,6 @@ afterAll(() => {
 describe('sendTestEmail function', () => {
   it('sends a test email with expected content', async () => {
     // create a test sender account using nodemailers 'etheral SMTP' service
-    let testAccount = await createTestAccount();
-    // reset environment variables
-    process.env.EMAIL_SERVICE = 'Ethereal';
-    process.env.EMAIL_USERNAME = testAccount.user;
-    process.env.EMAIL_PASSWORD = testAccount.pass;
 
     const { sendTestEmail } = require('../src/mailing/sender.js');
 
