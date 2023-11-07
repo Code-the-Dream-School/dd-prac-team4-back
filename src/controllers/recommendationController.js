1989162cafc5c87ccc3e501bc146b2a2dc7599e0
Yelena Japarova <88336329+DYA13@users.noreply.github.com>
2023-11-07 14:49:17 +0100

    Update src/controllers/recommendationController.js
    Co-authored-by: Akosua <9094098+akosasante@users.noreply.github.com>

--- src/controllers/recommendationController.js
+++ src/controllers/recommendationController.js
@@ -5,16 +5,9 @@ async function generateAlbumRecommendations(userId) {
 }
 const getAlbumRecsForUser = async (req, res) => {
   const { userId } = req.params;
-  try {
-    const recommendations =
-      await recommendationController.generateAlbumRecommendations(userId);
-    res.json(recommendations);
-  } catch (error) {
-    console.error('Error while generating recommendations:', error);
-    res
-      .status(500)
-      .json({ error: 'An error occurred while generating recommendations.' });
-  }
+  const recommendations =
+    await recommendationController.generateAlbumRecommendations(userId);
+  return res.json({ recommendations: recommendations });
 };
 
 module.exports = {
