4358637c87afd0482002489bab3558d0d5e842ad
Yelena Japarova <88336329+DYA13@users.noreply.github.com>
2023-11-07 16:17:42 +0100

    Update src/expressServer.js
    Co-authored-by: Akosua <9094098+akosasante@users.noreply.github.com>

--- src/expressServer.js
+++ src/expressServer.js
@@ -134,7 +134,7 @@ app.use('/api/v1/orders', orderRouter /* #swagger.tags = ['Orders'] */);
 app.use('/api/v1/reviews', reviewRouter /* #swagger.tags = ['Reviews'] */);
 app.use('/api/v1/wishlist', wishlistRoutes /* #swagger.tags = ['Wishlist'] */);
 app.use('/api/v1/chat', chatRoutes);
-app.use('api/v1/recommendations', recommendationRoutes);
+app.use('/api/v1/recommendations', recommendationRoutes);
 
 // Serve static files from the 'public' folder
 app.use('/admin', require('./routes/adminRoutes'));
