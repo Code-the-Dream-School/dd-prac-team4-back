9bb30ccc08ad02de1e2955b955474c0ef4ee7e25
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-11-14 14:12:56 +0100

    refactoring , code regarding imgur moved to imgur.js

--- src/expressServer.js
+++ src/expressServer.js
@@ -18,16 +18,9 @@ const { readFileSync } = require('fs');
 const { join } = require('path');
 const expressStaticGzip = require('express-static-gzip');
 const recommendationRoutes = require('./routes/recommendationRoutes');
-const fileUploadMiddleware = require('express-fileupload');
-const { ImgurClient } = require('imgur');
-const User = require('./models/User');
-const app = express();
+const imgur = require('./views/imgur/imgur');
 
-const imgurClient = new ImgurClient({
-  clientId: process.env.IMGUR_CLIENT_ID,
-  clientSecret: process.env.IMGUR_CLIENT_SECRET,
-  refreshToken: process.env.IMGUR_REFRESH_TOKEN,
-});
+const app = express();
 
 // Create a health endpoint for Render.com
 // This endpoint is used by Render.com to check if the backend is running
@@ -165,68 +158,11 @@ app.get('/tests', (req, res) => {
   const documentationFilePath = path.join(__dirname, 'public', 'tests.html');
   res.sendFile(documentationFilePath);
 });
-app.post(
-  '/api/v1/profile/:userId/uploadDB',
-  fileUploadMiddleware({ limits: { fileSize: 10000000 }, abortOnLimit: true }),
-  async (req, res) => {
-    console.log('Received request for /api/v1/profile/:userId/uploadDB');
-    const userId = req.params.userId;
-    console.log('User ID:', userId);
-    console.log('Entire req object:', req);
-    if (!userId) {
-      return res.status(400).send('No user id was provided.');
-    }
 
-    if (!req.files || Object.keys(req.files).length === 0) {
-      console.log('No files were uploaded.');
-      return res.status(400).send('No files were uploaded.');
-    }
-    if (!req.params.userId) {
-      return res.status(400).send('No user id was provided.');
-    }
-
-    const user = await User.findById(userId);
-    console.log('User:', user);
-    if (!user) {
-      return res.status(400).send('no user found');
-    }
-    const profile = req.files.profile;
-    console.log('File Details:', profile);
-
-    if (!profile) {
-      console.log('No profile file in the request.');
-      return res.status(400).send('No profile file in the request.');
-    }
-    console.log('File MIME Type:', profile.mimetype);
-    console.log('File Size:', profile.size);
-
-    if (!profile || !/^image/.test(profile.mimetype)) {
-      return res.status(400).send('File is not an image.');
-    }
-    if (!req.files || !req.files.profile) {
-      console.log('No profile file in the request.');
-      return res.status(400).send('No profile file in the request.');
-    }
-
-    const imgurRes = await imgurClient.upload({
-      image: profile.data.toString('base64'),
-      type: 'base64',
-      title: `${user._id}_profile_pic`,
-    });
-    if (imgurRes.success) {
-      user.profileImage = {
-        url: imgurRes.data.link,
-        altText: 'user profile picture',
-      };
-    }
-    res.status(200).send('Updated profile picture');
-    res.render('imgur/imgur', { userId: req.params.userId });
-  }
-);
 // Error handling middleware (**MUST** be defined _after_ all other routes and middleware)
 app.use(notFoundMiddleware); // Not found middleware to handle invalid routes
 app.use(errorHandlerMiddleware); // Error handler middleware
-
+app.use('/api/v1/profile', imgur);
 // ====== WEBSOCKET SETUP ======
 // put the express server definitions inside a more generic Node server so that we can reuse it for Socket.io
 const { Server } = require('socket.io');
---
+++ src/views/imgur/imgur.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const fileUploadMiddleware = require('express-fileupload');
+const { ImgurClient } = require('imgur');
+const User = require('../../models/User');
+
+const router = express.Router();
+const imgurClient = new ImgurClient({
+  clientId: process.env.IMGUR_CLIENT_ID,
+  clientSecret: process.env.IMGUR_CLIENT_SECRET,
+  refreshToken: process.env.IMGUR_REFRESH_TOKEN,
+});
+
+router.post(
+  '/:userId/uploadDB',
+  fileUploadMiddleware({ limits: { fileSize: 10000000 }, abortOnLimit: true }),
+  async (req, res) => {
+    console.log('Received request for /api/v1/profile/:userId/uploadDB');
+    const userId = req.params.userId;
+    console.log('User ID:', userId);
+    console.log('Entire req object:', req);
+    if (!userId) {
+      return res.status(400).send('No user id was provided.');
+    }
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+      console.log('No files were uploaded.');
+      return res.status(400).send('No files were uploaded.');
+    }
+    if (!req.params.userId) {
+      return res.status(400).send('No user id was provided.');
+    }
+
+    const user = await User.findById(userId);
+    console.log('User:', user);
+    if (!user) {
+      return res.status(400).send('no user found');
+    }
+    const profile = req.files.profile;
+    console.log('File Details:', profile);
+
+    if (!profile) {
+      console.log('No profile file in the request.');
+      return res.status(400).send('No profile file in the request.');
+    }
+    console.log('File MIME Type:', profile.mimetype);
+    console.log('File Size:', profile.size);
+
+    if (!profile || !/^image/.test(profile.mimetype)) {
+      return res.status(400).send('File is not an image.');
+    }
+    if (!req.files || !req.files.profile) {
+      console.log('No profile file in the request.');
+      return res.status(400).send('No profile file in the request.');
+    }
+
+    const imgurRes = await imgurClient.upload({
+      image: profile.data.toString('base64'),
+      type: 'base64',
+      title: `${user._id}_profile_pic`,
+    });
+    if (imgurRes.success) {
+      user.profileImage = {
+        url: imgurRes.data.link,
+        altText: 'user profile picture',
+      };
+    }
+    res.status(200).send('Updated profile picture');
+    res.render('../views/imgur', { userId: req.params.userId });
+  }
+);
+
+module.exports = router;
